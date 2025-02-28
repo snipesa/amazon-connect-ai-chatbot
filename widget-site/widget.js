ca4cb3d540aaaf287fc766d258c69776752eeac7
Etunyi Ashime <etunyiashime@gmail.com>
2025-02-28 09:58:22 -0500

    updates

--- widget-site/widget.js
+++ widget-site/widget.js
@@ -1,13 +1,3 @@
-<script type="text/javascript">
-  (function(w, d, x, id){
-    s=d.createElement('script');
-    s.src='https://dtn7rvxwwlhud.cloudfront.net/amazon-connect-chat-interface-client.js';
-    s.async=1;
-    s.id=id;
-    d.getElementsByTagName('head')[0].appendChild(s);
-    w[x] =  w[x] || function() { (w[x].ac = w[x].ac || []).push(arguments) };
-  })(window, document, 'amazon_connect', '627f3b16-0471-439b-8566-f3fb669f7c7a');
-  amazon_connect('styles', { iconType: 'CHAT', openChat: { color: '#ffffff', backgroundColor: '#e95607' }, closeChat: { color: '#ffffff', backgroundColor: '#123456'} });
-  amazon_connect('snippetId', 'QVFJREFIaWFZYXRVSlpIekdkUUg5YXhZenVQMktKRXNIWTVFQWpBYVErTEdzRnpvZHdGeXc0U3dUNUY3M0phcDBaNGlNTUQ0QUFBQWJqQnNCZ2txaGtpRzl3MEJCd2FnWHpCZEFnRUFNRmdHQ1NxR1NJYjNEUUVIQVRBZUJnbGdoa2dCWlFNRUFTNHdFUVFNQ1JCb3huQUluWnNrcEFGRUFnRVFnQ3ZzbFFUeGEwdzBtVnlTU3ZVWnpTdUVSOVFnbWw0RU8xczZlUm5HelRIdFhMeEZBbmVXNTBRbzdWUlM6Ok0wRzdQbXEyUWF4anhCTy9lRWIyR1RZL1EwS1BWd1RzcFcrM2NXS3FETGRTekZublEzcFkxM3M2UW9aM29Bdkd6aUVzT3A3TFNFa1ozMUJQTmR5eDQvYjFuWk9wakYrbVdRcDRXTDA1dDMrQU80c2M3VytZZnQxQ0NEUFVvb0VlYWlqYy9VRXR4YmU3cUFYRENwZ2NULytDY09xMFpXcz0=');
-  amazon_connect('supportedMessagingContentTypes', [ 'text/plain', 'text/markdown', 'application/vnd.amazonaws.connect.message.interactive', 'application/vnd.amazonaws.connect.message.interactive.response' ]);
-</script>
+
+// this is already included in the index.html
+// replace the values for YOUR_UNIQUE_ID and YOUR_SNIPPET_ID in index.html code for your contact flow 
